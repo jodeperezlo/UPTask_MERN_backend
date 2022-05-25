880c78b6f1532cf22a3a339585efa7d31a02a684
Jorge de Jesús Pérez López <jodeperezlo@ittepic.edu.mx>
2022-05-25 12:58:29 -0600

    [Update] Try view port

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ conectaDB();
 // let port = config.API_PORT || 8080;
 // app.set('port', port);
 app.set('port', config.PORT || 4000);
+console.log(`API running on port ${app.get('port')}`);
 
 // Configure CORS
 const whiteList = [config.FRONTEND_URL];
