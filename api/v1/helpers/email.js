ce4f8212e76ca831c9f4fc78cd34da665ece5176
Jorge de Jesús Pérez López <jodeperezlo@ittepic.edu.mx>
2022-05-25 22:46:58 -0600

    [Add] Separate socketIO

--- api/v1/helpers/email.js
+++ api/v1/helpers/email.js
@@ -168,7 +168,7 @@ export const sendEmailRegister = (data) => {
 													gap: 2rem;
 												"
 												><img
-													src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/73c3c356-39fb-4f0e-8140-7267efcb95b8/logo.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220418%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220418T151821Z&X-Amz-Expires=86400&X-Amz-Signature=20ad2938b169981b31cf53746a715b1189e9dd605cf04b891a75b875947f05d1&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D"logo.png"&x-id=GetObject"
+													src="https://raw.githubusercontent.com/jodeperezlo/UPTask_MERN_backend/master/assets/img/uptask.png"
 													width="32"
 													alt="Logo UpTask MERN"
 													style="
@@ -570,7 +570,7 @@ export const sendEmailForgotPassword = (data) => {
 													gap: 2rem;
 												"
 												><img
-													src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/73c3c356-39fb-4f0e-8140-7267efcb95b8/logo.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220418%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220418T151821Z&X-Amz-Expires=86400&X-Amz-Signature=20ad2938b169981b31cf53746a715b1189e9dd605cf04b891a75b875947f05d1&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D"logo.png"&x-id=GetObject"
+													src="https://raw.githubusercontent.com/jodeperezlo/UPTask_MERN_backend/master/assets/img/uptask.png"
 													width="32"
 													alt="Logo UpTask MERN"
 													style="
--- index.js
+++ index.js
@@ -1,44 +1,9 @@
+import { socketIO } from './socket/socketIO.js';
 import app from './app.js';
 import config from './config/config.js';
 
-const servidor = app.listen(app.get('port'));
+const server = app.listen(app.get('port'));
 
-// Socket.io
-import { Server } from 'socket.io';
-const io = new Server(servidor, {
-	pingTimeout: 60000,
-	cors: {
-		origin: '*',
-	},
-});
-
-io.on('connection', (socket) => {
-	// console.log('Connected to socket.io');
-
-	// ? Define los eventos
-	socket.on('open project', (project) => {
-		socket.join(project);
-	});
-
-	socket.on('new task', (task) => {
-		const project = task.project;
-		socket.to(project).emit('task added', task);
-	});
-
-	socket.on('delete task', (task) => {
-		const project = task.project;
-		socket.to(project).emit('task deleted', task);
-	});
-
-	socket.on('update task', (task) => {
-		const project = task.project._id;
-		socket.to(project).emit('task updated', task);
-	});
-
-	socket.on('change status task', (task) => {
-		const project = task.project._id;
-		socket.to(project).emit('status task changed', task);
-	});
-});
+socketIO(server);
 
 console.log(`Server is running on: ${config.API_URL}`);
